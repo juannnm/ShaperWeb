eb4d71083e21b6fca4dea9e90b319240e48c73ea
Juan <juan@squared.plus>
2022-10-26 13:03:40 -0300

    Footer added

---
+++ src/controller/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,73 @@
+import { Box, Divider, TextField, Typography, Link } from "@mui/material";
+import { Button } from "react-ui-controller";
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+
+
+
+export const Footer = () => {
+  return (
+    <Box>
+      <Box display="flex" flexDirection="column" textAlign='center' m='35px 25%'>
+        <Typography variant="h3">
+          We strive to deliver and ensure achivements beyond expected results.
+        </Typography>
+        <Button text="Register now" type="button" variant="contained" />
+      </Box>
+
+      <Divider/>
+      
+      <Box display='flex' flexDirection='row' justifyContent='center' alignItems='center' height='30vh'>
+        <Box display="flex" flexDirection="column"  justifyContent='center' alignContent='center' mr='100px'>
+          <Typography>Register To Our Newsletter</Typography>
+          <Box>
+            <TextField sx={{borderRadius:'100px', width:'300px', mr:'10px'}} size='small'/>
+            <Button text="L" type="button" variant="contained"/>
+          </Box>
+          <Box mt='20px' justifyContent='space-between'>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+            <FacebookOutlinedIcon fontSize="large"  sx={{mr:'18px'}}/>
+          </Box>
+        </Box>
+        <Box display="flex" flexDirection="row">
+          <Box display='flex' flexDirection='column' mr='30px'>
+            <Typography>Last seen</Typography>
+            <Link underline='none'>Motion Graphics</Link>
+            <Link underline='none'>UX/UI Design</Link>
+            <Link underline='none'>Business</Link>
+            <Link underline='none'>Nombre Curso</Link>
+            <Link underline='none'>Nombre Curso</Link>
+          </Box>
+          <Box display='flex' flexDirection='column' mr='30px'>
+            <Typography>Resources</Typography>
+            <Link underline='none'>REVISAR</Link>
+            <Link underline='none'>REVISAR</Link>
+            <Link underline='none'>REVISAR</Link>
+            <Link underline='none'>REVISAR</Link>
+            <Link underline='none'>REVISAR</Link>
+          </Box>
+          <Box display='flex' flexDirection='column'>
+            <Typography>Shaper</Typography>
+            <Link underline='none'>Academy</Link>
+            <Link underline='none'>Certified Careers</Link>
+            <Link underline='none'>Companies</Link>
+            <Link underline='none'>About Us</Link>
+            <Link underline='none'>Contact Us</Link>
+          </Box>
+        </Box>
+      </Box>
+      
+      <Divider/>
+      
+      <Box textAlign='center' m='20px' height='5vh'>
+        <Typography>
+          Copyright @ Shaper 2022. All Rights Reserved.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { EventCard } from '../controller/components/card/EventCard';
 import { ServiceCard } from '../controller/components/card/ServiceCard';
 import { CharacterCard } from '../controller/components/card/CharacterCard';
 import { TopAppBar } from '../controller/components/surfaces/TopAppBar/TopAppBar';
+import { Footer } from '../controller/components/surfaces/Footer/Footer';
 
 const Home: NextPage = () => {
     return (
@@ -23,28 +24,12 @@ const Home: NextPage = () => {
                 <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
             </Head>
-            {/* body {
-                    margin:0;
-                    }
-                    .wrapper {
-                        width: 100%;
-                        height: 100%;
-                        position: absolute;
-                        background: linear-gradient(45deg,#F17C58, #E94584, #24AADB , #27DBB1,#FFDC18, #FF3706);
-                        background-size: 600% 100%;
-                        animation: gradient 16s linear infinite;
-                        animation-direction: alternate;
-                    }
-                    @keyframes gradient {
-                        0% {background-position: 0%}
-                        100% {background-position: 100%}
-                    } */}
-            <Box component="main" sx={{ backgroundColor: '#ccc', height: '100vh', width: '100%' }}>
+            <Box component="main" sx={{ backgroundColor: '#ccc', width: '100%' }}>
                 <Box
                     sx={{
                         width: '100%',
-                        height: '75%',
-                        position: 'absolute',
+                        height: '600px',
+                        position: 'relative',
                         background: 'linear-gradient(45deg,#F17C58, #E94584, #24AADB , #27DBB1,#FFDC18, #FF3706)',
                         backgroundSize: '600% 100%',
                         animation: 'gradient 16s linear infinite',
@@ -56,7 +41,7 @@ const Home: NextPage = () => {
                     }}
                 ></Box>
                 <TopAppBar />
-
+                <Footer />
                 {/* <Box sx={{ padding: '10px', background: 'green' }}>
                     <CareerCard />
                 </Box>
