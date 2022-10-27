d07bd5e80494391dcc1a56f2d9966ca157c7ce81
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-10-26 22:43:21 -0300

    Minor fixes

--- src/controller/components/card/ShaperFor.tsx
+++ src/controller/components/card/ShaperFor.tsx
@@ -1,36 +1,36 @@
-import { Card } from "react-ui-controller";
-import { Button } from "react-ui-controller";
-import { Typography, Box } from "@mui/material";
+import { Card } from 'react-ui-controller';
+import { Button } from 'react-ui-controller';
+import { Typography, Box } from '@mui/material';
 
-
-export const ShaperFor = () => {
-  return (
-    <Card
-      background="transparent"
-      description={[
-        <Box key='container' sx={{alignItems:'center', justifyContent:'center'}}>
-          <Typography key='title' variant="h4" sx={{ textAlign: "center" }}>
-            SHAPER FOR
-          </Typography>
-          <Typography key='title' variant="h4" sx={{ textAlign: "center", fontWeight: 'bold'}}>
-            BUSINESS
-          </Typography>
-          <Typography key='paragraph' variant="body2" sx={{ textAlign: "center" }}>
-            If you are a company and would like to attrac the bes talent this is
-            your opportunity. Join Shaper Memberships for business and enjoy the
-            benefits
-          </Typography>
-        </Box>
-      ]}
-      direction="column"
-      width='300px'
-      height="248px"
-      actions={<Button text="Learn More" type="button" key='LearnMore' variant="contained"/>}
-    />
-  );
+interface IProps {
+    title: string;
+    titleBold: { text: string; color: string };
+    description: string;
 }
 
-
+export const ShaperFor = (props: IProps) => {
+    return (
+        <Card
+            background="transparent"
+            description={[
+                <Box key="container" sx={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography key="title" variant="h4" sx={{ textAlign: 'center' }}>
+                        {props.title && props.title.toUpperCase()}
+                    </Typography>
+                    <Typography key="title" variant="h4" sx={{ textAlign: 'center', fontWeight: 'bold', color: props.titleBold.color }}>
+                        {props.titleBold && props.titleBold.text.toUpperCase()}
+                    </Typography>
+                    <Typography key="paragraph" variant="body2" sx={{ textAlign: 'center' }}>
+                        {props.description}
+                    </Typography>
+                </Box>,
+            ]}
+            direction="column"
+            width="300px"
+            actions={<Button text="Learn More" type="button" key="LearnMore" variant="contained" />}
+        />
+    );
+};
 
 // Notas
 // EL boton queda al borde de la carta, consultar como solucionarlo
